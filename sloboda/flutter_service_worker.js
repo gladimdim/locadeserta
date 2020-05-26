8d89f708859a644550f89e6bf18dd6df7dd62ab6
Dmytro Gladkyi <gladimdim@gmail.com>
2020-05-26 22:50:33 +0300

    Experimental version with new Event affects.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -133,7 +133,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "36943ed4c3cd50d8f68a259695906fb4",
 "/": "36943ed4c3cd50d8f68a259695906fb4",
-"main.dart.js": "9c89414bc1f6f10213f3920895e6ada1",
+"main.dart.js": "c6c7ee3d950659519bdb3b35e221a61a",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
