776d984441961f09ae97c7266e49070109336800
Dmytro Gladkyi <gladimdim@gmail.com>
2020-05-28 22:33:04 +0300

    Added new Event.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -141,7 +141,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "36943ed4c3cd50d8f68a259695906fb4",
 "/": "36943ed4c3cd50d8f68a259695906fb4",
-"main.dart.js": "c3aa942f0bf66ba6cc8e533777617509",
+"main.dart.js": "7cc18bd71eaac329b34f25c8962a8fdf",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
