b3b635aaebcadf085477003dd649af1e2f9f5de5
Dmytro Gladkyi <gladimdim@gmail.com>
2020-06-07 23:36:20 +0300

    Auto save enabled for task-based.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -141,7 +141,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "36943ed4c3cd50d8f68a259695906fb4",
 "/": "36943ed4c3cd50d8f68a259695906fb4",
-"main.dart.js": "51d23757718a9e5fd2a86e73b44bbe63",
+"main.dart.js": "01ddcad998dab558e4550ff888622e0a",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
