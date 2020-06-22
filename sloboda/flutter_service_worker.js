ac41c0973e14d0d95e8fae92073b0fad32a31c7f
Dmytro Gladkyi <gladimdim@gmail.com>
2020-06-22 20:51:02 +0300

    Fixing sound for the web.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -142,7 +142,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "36943ed4c3cd50d8f68a259695906fb4",
 "/": "36943ed4c3cd50d8f68a259695906fb4",
-"main.dart.js": "ed77a572a5605d5f098f35b27c134bc7",
+"main.dart.js": "91f9b7a2f455b341a1f0ef00d1e4ff69",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
