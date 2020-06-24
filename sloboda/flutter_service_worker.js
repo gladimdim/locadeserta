f38d8bb42ab6efdce4812c92fb4ace524497a71a
Dmytro Gladkyi <gladimdim@gmail.com>
2020-06-24 16:40:03 +0300

    Added more sounds.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -143,7 +143,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "dd1dc7f415fbb659d9ac0e8cb2e9d7a2",
+"main.dart.js": "43b637b52ddd032e60e972347d34539c",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
