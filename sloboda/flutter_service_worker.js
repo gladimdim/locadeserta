cf33aae7c6ee0598d5f5aaac43d1c38cab88decc
Dmytro Gladkyi <gladimdim@gmail.com>
2020-06-28 20:07:15 +0300

    Fixed nextRandomEvents (de)serialization issue.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -145,7 +145,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "eec0b423e95fa198ae3f94b12f7b27c3",
+"main.dart.js": "f59a51b3a0935f80a2f731dd43bcac69",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
