7ed75ffc48a3621bc31c4ce1562c70b4ec26f8dc
Dmytro Gladkyi <gladimdim@gmail.com>
2020-07-02 16:38:28 +0300

    Added timestamp to saved slobodas.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -147,7 +147,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "a25340d59828af39382d64754967f87f",
+"main.dart.js": "8147fc2cd76a096641b3c8fa57f26aca",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
