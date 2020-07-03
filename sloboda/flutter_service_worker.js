054a7d52b2e97b0000a3f604d6ea43b1546cc19d
Dmytro Gladkyi <gladimdim@gmail.com>
2020-07-04 00:33:30 +0300

    Backend syncs.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -147,7 +147,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "506e37b07c10c5e9d8173299855c52dd",
+"main.dart.js": "98fcb88d744415c22daef9f46e4710ca",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
