1004ce911bb69da124b2907ff95b167089626718
Dmytro Gladkyi <gladimdim@gmail.com>
2020-07-03 18:39:04 +0300

    Enhanced paddings. Fixed state update bugs.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -147,7 +147,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "6b8f78270fff7c9b2041850b74de4c42",
+"main.dart.js": "506e37b07c10c5e9d8173299855c52dd",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
