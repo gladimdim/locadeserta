66fc431cb6386095ac459a6f2cfbfc1692cc2d32
Dmytro Gladkyi <gladimdim@gmail.com>
2020-07-05 10:34:56 +0300

    Enhanced Sync View.
    Added progress indicators and translations.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -147,7 +147,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "81439cbce17c824db5ace97acc40f047",
+"main.dart.js": "5f8f113d49893468e8c7024dee0b4c1f",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
