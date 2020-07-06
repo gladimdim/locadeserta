495ddbfc34c3e7b7f75c0d39966eb3ede3d48432
Dmytro Gladkyi <gladimdim@gmail.com>
2020-07-06 22:52:55 +0300

    Auto-save feature.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -147,7 +147,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "8776c02b3c696ba0b8ff32763ed9669d",
+"main.dart.js": "4b585ec3b2dd18c0428d3a12c934cf31",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
