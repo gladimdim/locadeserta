90fb9233c38a512b4f5992fe499df11f08991904
Dmytro Gladkyi <gladimdim@gmail.com>
2020-07-24 23:17:48 +0300

    UI changes.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -321,7 +321,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "fc914324a4bdf7e5910162295a818e9f",
+"main.dart.js": "448ece7746c05ff0d7f2ecc0b7b58507",
 "manifest.json": "54cbd107fea365ff4c20a64e16430d08"
 };
 
