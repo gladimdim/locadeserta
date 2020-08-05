05ae07334046fc546dad4e6a7e281a94a87371a6
Dmytro Gladkyi <gladimdim@gmail.com>
2020-08-05 17:10:53 +0300

    Error reporint enhancements.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -324,7 +324,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "78d1aa896ca37b6138a4929494c0e7a7",
+"main.dart.js": "f7237ca0ab0bcf5b81f29e62e079bcde",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a"
 };
 
