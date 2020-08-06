c120e4a797ceaf05c7e8ec883a0b5b25c3d51850
Dmytro Gladkyi <gladimdim@gmail.com>
2020-08-06 23:19:55 +0300

    Error reporting improved.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -324,7 +324,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "bf3319c754dbe676c5423cf53fe9e88b",
+"main.dart.js": "1af7393e836558bb89896a2b23ce3bde",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a"
 };
 
