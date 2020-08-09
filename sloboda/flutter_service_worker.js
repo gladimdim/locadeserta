263806426c549da50e8f3fc5fd149baf39a077fd
Dmytro Gladkyi <gladimdim@gmail.com>
2020-08-09 23:33:07 +0300

    Implemented Field and Forest as embedded views in settlement canvas.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "b67bcddddcef331143b0d4dc6b5bc816",
+  "assets/AssetManifest.json": "c5996094b763ce6a2d2143752bfb9ac7",
 "assets/assets/city_names.json": "abfc1152f5a2afa6f9d727244c902168",
 "assets/assets/sounds/event_add.mp3": "0bf025b38b381eb092c1bbc2917ddb1a",
 "assets/assets/sounds/event_choicable_add.mp3": "6881bf070ba8c5a7555e84e890d06a80",
@@ -149,6 +149,8 @@ const RESOURCES = {
 "assets/images/background/landing/c_5.jpg": "3a30dff864f014e5b77722421e59c3b1",
 "assets/images/background/landing/c_6.jpg": "48167f5d9435666ca4939ceb8bf51f23",
 "assets/images/background/landing/c_7.jpg": "582c6ca7853ec0f40849e8213b912c50",
+"assets/images/background/parallax/field.png": "b043deabe0fa28af4f52b0492144fb7d",
+"assets/images/background/parallax/forest.png": "63d4199d9343d39e8932cc9999bc7d1e",
 "assets/images/background/parallax/goxel_background.png": "4d25341b3d9edcdcf9803d60f5582519",
 "assets/images/background/parallax/steppe.jpg": "0bb9ab4bb286f316355785d0364eecfb",
 "assets/images/background/parallax/steppe_2.jpg": "abf58404e0e2bb4d2a5e2e0d71600eb3",
@@ -326,7 +328,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "49f141fefcc99492e3db9f5203f44bfb",
+"main.dart.js": "fe88ea20ef18ebedf1b70c2d34d24d62",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a"
 };
 
