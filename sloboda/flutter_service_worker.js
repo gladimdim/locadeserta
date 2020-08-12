fb6097198cf1cb348a73885346423631f42e82dd
Dmytro Gladkyi <gladimdim@gmail.com>
2020-08-12 16:22:27 +0300

    Added iron mine and Trappers house
    Changed layout. Now built and not yet built buildings are rendered in the same list.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "3e848a0c4d630a2198635216a11ac2c0",
+  "assets/AssetManifest.json": "2276a451169f05fe46d4385a35d451f5",
 "assets/assets/city_names.json": "abfc1152f5a2afa6f9d727244c902168",
 "assets/assets/sounds/event_add.mp3": "0bf025b38b381eb092c1bbc2917ddb1a",
 "assets/assets/sounds/event_choicable_add.mp3": "6881bf070ba8c5a7555e84e890d06a80",
@@ -153,10 +153,11 @@ const RESOURCES = {
 "assets/images/background/parallax/forest.png": "1b61d165e438039766d2d8e30d869181",
 "assets/images/background/parallax/goxel_background.png": "4d25341b3d9edcdcf9803d60f5582519",
 "assets/images/background/parallax/house.png": "6732039c56e0a0aa31b6bb04e0a093ca",
-"assets/images/background/parallax/imgui.ini": "225ba9004e939001679ded34841a0d7a",
+"assets/images/background/parallax/iron_mine.png": "7d6ad532bb14e2f005d7368c9b3954a5",
 "assets/images/background/parallax/river.png": "dc6eb2d08ca2ba8c44c39a353299185f",
 "assets/images/background/parallax/steppe.jpg": "0bb9ab4bb286f316355785d0364eecfb",
 "assets/images/background/parallax/steppe_2.jpg": "abf58404e0e2bb4d2a5e2e0d71600eb3",
+"assets/images/background/parallax/trappershouse.png": "d49df3887a390c7f26603a8190962f28",
 "assets/images/background/river/0.jpg": "a3345d42fab01efa74a8b8a14f361219",
 "assets/images/background/river/1.jpg": "e49b1d2d0b61c1e2e690b531ca502b51",
 "assets/images/background/river/10.jpg": "28eb0031fc4e5be5859aa4b1f1540e08",
@@ -331,7 +332,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "ce2bf2482973e4ab153f9aa6cf43426f",
+"main.dart.js": "4feecb699c4ef7510ed66f10f6ba3772",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a"
 };
 
