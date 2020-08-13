8986f4817cfa29a25a590b4ca04ac411e5b718e6
Dmytro Gladkyi <gladimdim@gmail.com>
2020-08-13 16:29:11 +0300

    Fixed 1 second interval update.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "320545cc3a957dafa7b062c53c759f21",
+  "assets/AssetManifest.json": "f2636c3056d530979464161b54308c3e",
 "assets/assets/city_names.json": "abfc1152f5a2afa6f9d727244c902168",
 "assets/assets/sounds/event_add.mp3": "0bf025b38b381eb092c1bbc2917ddb1a",
 "assets/assets/sounds/event_choicable_add.mp3": "6881bf070ba8c5a7555e84e890d06a80",
@@ -154,6 +154,7 @@ const RESOURCES = {
 "assets/images/background/parallax/forest.png": "1b61d165e438039766d2d8e30d869181",
 "assets/images/background/parallax/goxel_background.png": "4d25341b3d9edcdcf9803d60f5582519",
 "assets/images/background/parallax/house.png": "6732039c56e0a0aa31b6bb04e0a093ca",
+"assets/images/background/parallax/imgui.ini": "225ba9004e939001679ded34841a0d7a",
 "assets/images/background/parallax/iron_mine.png": "7d6ad532bb14e2f005d7368c9b3954a5",
 "assets/images/background/parallax/mill.png": "7c2bc326fba96c0c3811e702ce6663e7",
 "assets/images/background/parallax/powder_cellar.png": "17e252293a45fae37d122e4e5d39cc53",
@@ -338,7 +339,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "80e5599a7d8121d8e4e26fe835d7e2d2",
+"main.dart.js": "69c764522851588c2d74cba3b054ae01",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a"
 };
 
