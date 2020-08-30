85cdb23d86ef7f6a21da9330385623fddf4d307a
Dmytro Gladkyi <gladimdim@gmail.com>
2020-08-30 14:24:48 +0300

    Added Tower and Watch Tower level art.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "51b817b17e20821c21945d45ea2b4b50",
+  "assets/AssetManifest.json": "39f055446e4abf0e4cf34ff28b736de7",
 "assets/assets/city_names.json": "abfc1152f5a2afa6f9d727244c902168",
 "assets/assets/sounds/event_add.mp3": "0bf025b38b381eb092c1bbc2917ddb1a",
 "assets/assets/sounds/event_choicable_add.mp3": "6881bf070ba8c5a7555e84e890d06a80",
@@ -190,13 +190,18 @@ const RESOURCES = {
 "assets/images/background/parallax/steppe.jpg": "0bb9ab4bb286f316355785d0364eecfb",
 "assets/images/background/parallax/steppe_2.jpg": "abf58404e0e2bb4d2a5e2e0d71600eb3",
 "assets/images/background/parallax/tower.png": "f14caaab1015480f98466db6aec1f211",
+"assets/images/background/parallax/tower_1.png": "fec2b43656730eae2b4cda5304e1fa6d",
+"assets/images/background/parallax/tower_2.png": "710c1712d1b536fec13a74e1d912c39c",
+"assets/images/background/parallax/tower_3.png": "0dbec1d8604dd72e8b23ccd81b538269",
 "assets/images/background/parallax/trappershouse_1.png": "9e236c2adc19221dd9d9e8163216bdb4",
 "assets/images/background/parallax/trappershouse_2.png": "755fd520b6cfe5ec3948259172951675",
 "assets/images/background/parallax/trappers_house_3.png": "5f69d3da8c353b8d2b324b475ab06b7a",
 "assets/images/background/parallax/wall_1.png": "4336f7fcefe73ee82d9c9117d851aacb",
 "assets/images/background/parallax/wall_2.png": "2ddce9d3f729e67815752973d846c03b",
 "assets/images/background/parallax/wall_3.png": "3c4934e97fdbdc70de79633c369c7a73",
-"assets/images/background/parallax/watch_tower.png": "7b5cee9c5fba63fa13456b5183cb7bdb",
+"assets/images/background/parallax/watch_tower_1.png": "ef559e8271bd2ce76d3dea88618816da",
+"assets/images/background/parallax/watch_tower_2.png": "7b5cee9c5fba63fa13456b5183cb7bdb",
+"assets/images/background/parallax/watch_tower_3.png": "db90cff662d9eb97cf74dc064c2eecde",
 "assets/images/background/river/0.jpg": "a3345d42fab01efa74a8b8a14f361219",
 "assets/images/background/river/1.jpg": "e49b1d2d0b61c1e2e690b531ca502b51",
 "assets/images/background/river/10.jpg": "28eb0031fc4e5be5859aa4b1f1540e08",
@@ -371,7 +376,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "931b205bf9e775364aa1678fc97d4f24",
+"main.dart.js": "2efd023685457a41c65c288f39e812fb",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a"
 };
 
