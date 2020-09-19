a320f29723d99fb63be181e6d380775c98c97bb8
Dmytro Gladkyi <gladimdim@gmail.com>
2020-09-19 14:09:58 +0300

    Added all events to the map.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -386,7 +386,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "9b1f02107086958a2ded9e82a71ed661",
+"main.dart.js": "f59932f074b7f8f40a0e92c8efbbf632",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a",
 "version.json": "c1a8239ba546493e73ad640dad8ceddb"
 };
