16fb6827ff8a2efec9b7624e76cad948d20edd74
Dmytro Gladkyi <gladimdim@gmail.com>
2020-09-21 16:46:08 +0300

    Events on map are now save and restored.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -386,7 +386,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "d223305a0e6918c9cc22b2c0086c0695",
+"main.dart.js": "e9dc4216428791b2dbb6e66c88770815",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a",
 "version.json": "c1a8239ba546493e73ad640dad8ceddb"
 };
