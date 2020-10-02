6ef6a69d8742f46b95aabc19d543fc0484e3a1e0
Dmytro Gladkyi <gladimdim@gmail.com>
2020-10-02 13:40:41 +0300

    Fixed a bug when decisions for Events were not rendered on screen.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -416,7 +416,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "96dbde71f48c968d727e2dddd4b84244",
+"main.dart.js": "eaf2ef858d8176c12e9f4b8a7fcf7dea",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a",
 "version.json": "c1a8239ba546493e73ad640dad8ceddb"
 };
