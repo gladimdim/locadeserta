1baca88ef596e2346eea4a5989818c5726682586
Dmytro Gladkyi <gladimdim@gmail.com>
2020-10-06 10:27:42 +0300

    Added Full Production Chain render.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -416,7 +416,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "3bfab64d905754fffa64981c87e9014e",
+"main.dart.js": "ecaf1172a812bcf6f4076bf1a7d10122",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a",
 "version.json": "c642afdbdd8b5190e4049953d95bb893"
 };
