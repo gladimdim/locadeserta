f44e65a50b25dfc7610300178163fc7d0bce11df
Dmytro Gladkyi <gladimdim@gmail.com>
2020-10-07 22:02:00 +0300

    Enhanced app bar title height.
    Replaced task complete sound with another more pleasent.

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "523da284e55b782532d6bb9b1d76f52f",
+  "assets/AssetManifest.json": "67d08036e693714503a5ee2eab6323f9",
 "assets/assets/city_names.json": "abfc1152f5a2afa6f9d727244c902168",
 "assets/assets/sounds/event_add.mp3": "0bf025b38b381eb092c1bbc2917ddb1a",
 "assets/assets/sounds/event_choicable_add.mp3": "6881bf070ba8c5a7555e84e890d06a80",
 "assets/assets/sounds/event_choicable_complete.mp3": "af822329b6519b80e7aad7a5a13e85e9",
 "assets/assets/sounds/event_complete.mp3": "65d523a9b579bec58d710f0380b862de",
 "assets/assets/sounds/new_year.mp3": "05592ee0fadfa1ac9ba3c4e3c104bb75",
-"assets/assets/sounds/task_complete.mp3": "c33de586a860243c12a6c209ccce0d7e",
+"assets/assets/sounds/task_complete.mp3": "fcd7e4b18857c15f56de5a238a6030e5",
 "assets/FontManifest.json": "ce7e5ed330de171fd19dfb6a7f56fc8f",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/fonts/PressStart2P-Regular.ttf": "2c404fd06cd67770807d242b2d2e5a16",
@@ -416,7 +416,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "62c56c72ca95178258e870faf379e271",
+"main.dart.js": "e75a8b5c3f7bc17bd68d64b0db6161e7",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a",
 "version.json": "c642afdbdd8b5190e4049953d95bb893"
 };
