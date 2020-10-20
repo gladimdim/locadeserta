2cf54bd94f30e18732b3b0593b37f41ee09dd0b5
Dmytro Gladkyi <gladimdim@gmail.com>
2020-10-20 21:52:06 +0300

    Added color pickers.

--- family_dashboard/flutter_service_worker.js
+++ family_dashboard/flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "e1f3f0ac4853d6e767ca2accb8386842",
 "/": "e1f3f0ac4853d6e767ca2accb8386842",
-"main.dart.js": "5c2ceca03e5fe6dc66a2364ce8e6e064",
+"main.dart.js": "217fc5afa4402a262c307149fae3a9af",
 "manifest.json": "97c9c73df5232b6d3eef5192840c4e66",
 "version.json": "e2b69c206f4b1aa3750659a550833a2a"
 };
