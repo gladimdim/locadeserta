0f7208eefe4f842f34ac1772de113c21f25de5f2
Dmytro Gladkyi <gladimdim@gmail.com>
2020-10-26 15:48:43 +0200

    Increased default citizen count to 30

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -436,7 +436,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "5ddb4c490da85af6adde0801a0f62f68",
+"main.dart.js": "90b02baa841a1464fe086cfce0902e2b",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a",
 "version.json": "4300548c8ef31dbe39afde1bd22d315d"
 };
