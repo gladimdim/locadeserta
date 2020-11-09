b806b9d00162cbce3707704b15cf42593c555e74
Dmytro Gladkyi <gladimdim@gmail.com>
2020-11-09 16:46:31 +0200

    Added Affected Buildings and Map Tiles to the research overview

--- sloboda/flutter_service_worker.js
+++ sloboda/flutter_service_worker.js
@@ -662,7 +662,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2bda3d471e94c5c1f221d54a0cbb9295",
 "/": "2bda3d471e94c5c1f221d54a0cbb9295",
-"main.dart.js": "f01f3263f6f3da5fbde31ad50019d0a3",
+"main.dart.js": "47ba7a25061479eafab4b5de3364c545",
 "manifest.json": "4a1ced060193f4539b0cebff0f8dae1a",
 "version.json": "13090907d1f540b3dec8dec8899ce876"
 };
