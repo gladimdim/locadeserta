664a8715c093018150a1350c0bf65dfb7123fdf8
Dmytro Gladkyi <gladimdim@gmail.com>
2020-11-24 18:20:08 +0200

    Added share button at the end of story section.

--- game/flutter_service_worker.js
+++ game/flutter_service_worker.js
@@ -201,7 +201,7 @@ const RESOURCES = {
 "assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
 "index.html": "bdb59328970caa2ff0f8e9e6da1be7b1",
 "/": "bdb59328970caa2ff0f8e9e6da1be7b1",
-"main.dart.js": "29325666a1e6a03b46bc5778ff7b7964",
+"main.dart.js": "d64f9ada2c2584887a7384b27655021d",
 "version.json": "7a4df6d2e5d49f5aa14e722cc1cd0153"
 };
 
