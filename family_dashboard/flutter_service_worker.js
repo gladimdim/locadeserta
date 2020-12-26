78b6b454dbc8c1a7a11306a45a23652513f7a3d0
Dmytro Gladkyi <gladimdim@gmail.com>
2020-12-26 22:09:34 +0200

    Enhanced colors for days, tasks.

--- family_dashboard/flutter_service_worker.js
+++ family_dashboard/flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "e1f3f0ac4853d6e767ca2accb8386842",
 "/": "e1f3f0ac4853d6e767ca2accb8386842",
-"main.dart.js": "32bf6df7df15aa7f5920b45a976df2a0",
+"main.dart.js": "b77672bcb2f57b3df8193931a6769ed6",
 "manifest.json": "97c9c73df5232b6d3eef5192840c4e66",
 "version.json": "e2b69c206f4b1aa3750659a550833a2a"
 };
