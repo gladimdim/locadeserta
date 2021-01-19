47f1f8c6288a78885f3d72f37730d3bf5a4023e0
Dmytro Gladkyi <gladimdim@gmail.com>
2021-01-19 11:54:52 +0200

    Removed prefix hex

--- hex/flutter_service_worker.js
+++ hex/flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1b1478e22f5c076a2b85f8f412814d48",
-"/": "1b1478e22f5c076a2b85f8f412814d48",
+"index.html": "0f20e5c5306bbdecda6b492e9abf72d8",
+"/": "0f20e5c5306bbdecda6b492e9abf72d8",
 "main.dart.js": "d3cdcc25e837d2d5f37cbb6f38c1b726",
 "manifest.json": "70293e76ed441e5915d49ca32de5e287",
 "version.json": "3641ee9c377e125686b4e5b1824d06b6"
