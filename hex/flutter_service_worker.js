a7bd66254ca5fc2026b5bf89d4bada400cc6e72a
Dmytro Gladkyi <gladimdim@gmail.com>
2021-01-21 18:23:05 +0200

    Added requirements for each resource.

--- hex/flutter_service_worker.js
+++ hex/flutter_service_worker.js
@@ -49,7 +49,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2436fc33214b9f22bb65bb41ad34ca09",
 "/": "2436fc33214b9f22bb65bb41ad34ca09",
-"main.dart.js": "ddd4ada4c51b9da0e66d62beeee38275",
+"main.dart.js": "ffe4e433b5368eb6792ffa9a166ca5e7",
 "manifest.json": "70293e76ed441e5915d49ca32de5e287",
 "version.json": "3641ee9c377e125686b4e5b1824d06b6"
 };
