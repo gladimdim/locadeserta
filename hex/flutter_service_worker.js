b7e0dba5771c78de45cfcc17d4d9908435d66c61
Dmytro Gladkyi <gladimdim@gmail.com>
2021-02-06 21:48:57 +0200

    Fixed an issue when howler played sound at wrong path

--- hex/flutter_service_worker.js
+++ hex/flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "version.json": "3641ee9c377e125686b4e5b1824d06b6",
 "index.html": "1b82743ba88495b30dd5f68572681f71",
 "/": "1b82743ba88495b30dd5f68572681f71",
-"main.dart.js": "e45dda46c3e723c49bf01b330a529a74",
+"main.dart.js": "35640323175d8f8ef3c8642918a0ae49",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
