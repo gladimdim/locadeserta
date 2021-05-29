0efcf406468bcda7147bb8958d8bc7af42cfb18a
Dmytro Gladkyi <gladimdim@gmail.com>
2021-05-29 22:41:06 +0300

    Chumaki: Level up for Leader.

--- chumaki/flutter_service_worker.js
+++ chumaki/flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "version.json": "5f6dcf92ae94738bc4b9255faa096d74",
 "index.html": "5889d2738ed0034b3211e8e3b14a4d3d",
 "/": "5889d2738ed0034b3211e8e3b14a4d3d",
-"main.dart.js": "c3f5c4de74b92f56853ec4f8a7a96dc7",
+"main.dart.js": "20f9ce402ac19490a833774a4de7ab50",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
